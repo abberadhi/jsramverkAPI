26fc78c285a7d7ca5c8930ed9f9ea3de2fb36112
abberadhi <abbe.radhi11@gmail.com>
2021-09-20 12:02:13 +0200

    db uri change

--- src/db/database.js
+++ src/db/database.js
@@ -8,7 +8,8 @@ const database = {
         let dsn = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.vhks9.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 
         if (process.env.NODE_ENV === 'test') {
-            dsn = "mongodb+srv://abbe:hIukECQkbh9nTTFE@cluster0.vhks9.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+            dsn = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.vhks9.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
         }
 
         const client  = await mongo.connect(dsn, {
