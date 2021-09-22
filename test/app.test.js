1c6063a1be35eee698ea4719a90e75e9c7024485
abberadhi <abbe.radhi11@gmail.com>
2021-09-22 23:16:08 +0200

    wrote tests for api

--- test/app.test.js
+++ test/app.test.js
@@ -16,50 +16,114 @@ after(async() => {
 })
 
 describe('app', () => {
-    describe('get /', () => {
-        it('test index hello world', (done) => {
-            chai.request(server)
-                .get("/")
-                .end((err, res) => {
-                    res.should.have.status(200);
-                    res.text.should.be.a("string");
-                    
-                    done();
-                });
-        });
-    });
-});
-
-// rewrite
-describe('app', () => {
-    describe('POST /', () => {
-        it('test fetching all data', (done) => {
+    describe('POST /findall', () => {
+        it('should be nothing in the result', (done) => {
             chai.request(server)
                 .post("/findall")
                 .end((err, res) => {
                     res.should.have.status(200);
-                    res.text.should.be.a("string");
-
-                    console.log(res);
+                    res.body.should.be.an("array");
+                    res.body.length.should.be.below(1);
 
                     done();
                 });
         });
     });
-});
 
-describe('app', () => {
-    describe('POST /create', () => {
-        it('creating new ', (done) => {
+    describe('POST /update', () => {
+        it('did not specify id so should create new doc', (done) => {
             chai.request(server)
                 .post("/update")
                 .send({})
                 .end((err, res) => {
                     res.should.have.status(201);
-                    res.text.should.be.a("string");
+                    console.log(res.body);
+                    res.body.acknowledged.should.equal(true);
+                    res.body.insertedId.should.be.an("string");
+
+                    done();
+                });
+        });
+    });
+
+    let entryId;
+
+    describe('POST /findall', () => {
+        it('accoring to previous test there should be exactly one untitled entry', (done) => {
+            chai.request(server)
+                .post("/findall")
+                .send({})
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    res.body.length.should.equal(1);
+                    res.body[0].name.should.equal("Untitled");
+
+                    entryId = res.body[0]._id;
+
+                    done();
+                });
+        });
+    });
+
+    describe('POST /find', () => {
+        it('get the untitled entry by id', (done) => {
+            chai.request(server)
+                .post("/find")
+                .send({"id": entryId})
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    res.body.name.should.equal("Untitled");
+
+                    done();
+                });
+        });
+    });
+
+    describe('POST /update', () => {
+        it('update the content of the document and look for modified docs', (done) => {
+            chai.request(server)
+                .post("/update")
+                .send({"id": entryId, "name": "new name", "content": "<h1>hello</h1>"})
+                .end((err, res) => {
+                    // console.log(res);
+                    res.should.have.status(201);
+                    res.body.acknowledged.should.equal(true);
+                    res.body.modifiedCount.should.equal(1);
+
+                    done();
+                });
+        });
+    });
+
+    describe('POST /find', () => {
+        it('check that doc is updated with correct content', (done) => {
+            chai.request(server)
+                .post("/find")
+                .send({"id": entryId})
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    res.body.name.should.equal("new name");
+                    res.body.content.should.equal("<h1>hello</h1>");
+
+                    done();
+                });
+        });
+    });
+
+    describe('POST /delete', () => {
+        it('delete a doc by id', (done) => {
+            chai.request(server)
+                .post("/delete")
+                .send({"id": entryId})
+                .end((err, res) => {
+                    res.should.have.status(201);
+
+                    res.body.deletedCount.should.equal(1);
+                    res.body.acknowledged.should.equal(true);
 
                     done();
                 });
         });
     });
 });
+
