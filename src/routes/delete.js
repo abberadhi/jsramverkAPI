27453c997e48b9518d1e09bd289d04f59377b24d
abberadhi <abbe.radhi11@gmail.com>
2021-10-07 23:11:09 +0200

    impmenented delete

--- src/routes/delete.js
+++ src/routes/delete.js
@@ -1,20 +1,22 @@
 const e = require('express');
 var express = require('express');
 var router = express.Router();
+let Doc = require('../models/Doc');
 
 var crud = require('../models/crud');
 
 /**
  * expecting id (optional), name, content 
  */
-router.post("/", async (req, res) => {
-
+router.delete("/", async (req, res) => {
     try {
-        let output = await crud.delete(req.body);
-        res.status(201).json(output);
-    } catch (e) {
-        res.status(404).json({msg: "could not find what u were looking for", error: e})
-    }
+        const document = await Doc.findByIdAndDelete(req.body.id);
+        if (!document) throw Error('No document found');
+
+        res.status(200).json({ data: document, success: true });
+      } catch (e) {
+        res.status(400).json({ msg: e.message, success: false });
+      }
 });
 
 module.exports = router;
